429091cbf60682b92df430218ec30b779b1ffd0f
Gennady Petukhov <g3pmail@gmail.com>
2021-06-01 08:55:04 +0300

    Add Scaffold

--- 01-FirstApp/first_app/lib/main.dart
+++ 01-FirstApp/first_app/lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
+// Hot reload will NOT work for this app.
+
 // Add commas after each round bracket
 // (this will help automatically format code)
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
       ),
     ),
   );
