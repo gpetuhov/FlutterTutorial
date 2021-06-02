bb592e75b83106e451de81478b4b7f407fe5556f
Gennady Petukhov <g3pmail@gmail.com>
2021-06-02 10:45:54 +0300

    Add headline

--- 03-MiCard/mi_card_flutter/lib/main.dart
+++ 03-MiCard/mi_card_flutter/lib/main.dart
@@ -26,6 +26,16 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.normal,
                 ),
               ),
+              Text(
+                'ANDROID AND FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 15.0,
+                  color: Colors.teal.shade100,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+              ),
             ],
           ),
         ),
